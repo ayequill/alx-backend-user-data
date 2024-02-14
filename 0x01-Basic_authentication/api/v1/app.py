db2edd465bba003b7200ec106ec160e414373b0e
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-14 00:25:26 +0000

    Create a custom error handler

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,6 +27,12 @@ def unauthorized_exception(error) -> Tuple[Any, int]:
     return jsonify({'error': 'Unauthorized'}), 401
 
 
+@app.errorhandler(403)
+def forbidden_exception(error):
+    """Returns a forbidden error"""
+    return jsonify({'error': 'Forbidden'}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def protected():
     """ Raise a 401 """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """ raise a 403 """
+    abort(403)
