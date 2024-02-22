0b2aea27d664b952aa0d409f8417ff44a2d585f2
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-22 21:02:56 +0000

    basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 """ A basic flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/')
 def index():
     """ Return a basic json"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """A users route """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email and password:
+        try:
+            AUTH.register_user(email, password)
+            return jsonify({"email": email, "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
+    else:
+        return jsonify({"message": "email and password is required"}), 400
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Module for Auth class """
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -27,3 +27,11 @@ class Auth:
         except NoResultFound:
             new_user = self._db.add_user(email, _hash_password(password))
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check valid login"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
